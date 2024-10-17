1c0e7314fe569e9db0dd60d11eaf2d5a11e8038e
radcli14 <radcli14@gmail.com>
2024-10-17 16:56:40 +0200

    added a boundary condition file

--- aerosplat/__init__.py
+++ aerosplat/__init__.py
@@ -1,5 +1,6 @@
 from .symbolic import *
 from .aerosplat import *
+from .boundary import LineBoundary
 
 def init_test():
     return True
---
+++ aerosplat/boundary.py
@@ -0,0 +1,22 @@
+# Create Boundary Condition Class
+import numpy as np
+import random
+
+class LineBoundary:
+    point0 = np.zeros(3)
+    point1 = np.zeros(3)
+    velocity = np.zeros(3)
+    
+    def __init__(self, point0=[0, 0, 0], point1=[0, 0, 0], velocity=[0, 0, 0]):
+        self.point0 = np.array(point0)
+        self.point1 = np.array(point1)
+        self.velocity = np.array(velocity)
+        
+    def __repr__(self):
+        return f"LineBoundary(point0={self.point0}, point1={self.point1}, velocity={self.velocity})"
+    
+    def point_at(self, weighted_distance):
+        return (1 - weighted_distance) * self.point0 + weighted_distance * self.point1
+    
+    def point_at_random(self):
+        return self.point_at(random.uniform(0, 1))
